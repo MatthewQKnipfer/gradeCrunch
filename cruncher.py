ce6a3f72e928f28dd513a63eef4f754d6dc9aaa3
Matthew Q. Knipfer <mknipfer@highpoint.edu>
2019-02-19 09:20:03 -0500

    Try (and fail) some requests for user input

--- cruncher.py
+++ cruncher.py
@@ -1,19 +1,49 @@
 # author: Matthew Q. Knipfer
 
+# Prompt do you have?
 
-# Prompt: Do you have?
 
 # Homework
+homeworkYN = input("Do you have homework (yes or no): ")
 
+    if homeworkYN == "yes":
+        x = 1
+    else:
+        x = 2
 
 # Quizzes
+quizYN = input("Do you have quizzes (yes or no): ")
 
-
+    if quizYN == "yes":
+        x = 1
+    else:
+        x = 2
 # Tests
+testYN = input("Do you have tests (yes or no): ")
 
+    if testYN == "yes":
+        x = 1
+    else:
+        x = 2
 
 # Projects
+projectYN = input("Do you have tests (yes or no): ")
 
+    if projectYN == "yes":
+        x = 1
+    else:
+        x = 2
 
 # Final
+finalYN = input("Do you have quizzes (yes or no): ")
+
+    if finalYN == "yes":
+        x = 1
+    else:
+        x = 2
+
+
+
+
+# Allocation of pie
 
