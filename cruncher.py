8bb4fd061c2166ed9152f873f585149a61c99364
Matthew Q. Knipfer <mknipfer@highpoint.edu>
2019-02-20 16:16:45 -0500

    Fix indentation error

--- cruncher.py
+++ cruncher.py
@@ -1,41 +1,44 @@
 # author: Matthew Q. Knipfer
 
+
+
 # Prompt do you have?
 
-
 # Homework
-homeworkYN = input("Do you have homework (yes or no): ")
+
+def have():
+    homeworkYN = input("Do you have homework (yes or no): ")
 
     if homeworkYN == "yes":
         x = 1
     else:
         x = 2
 
-# Quizzes
-quizYN = input("Do you have quizzes (yes or no): ")
+    # Quizzes
+    quizYN = input("Do you have quizzes (yes or no): ")
 
     if quizYN == "yes":
         x = 1
     else:
         x = 2
-# Tests
-testYN = input("Do you have tests (yes or no): ")
+    # Tests
+    testYN = input("Do you have tests (yes or no): ")
 
     if testYN == "yes":
         x = 1
     else:
         x = 2
 
-# Projects
-projectYN = input("Do you have tests (yes or no): ")
+    # Projects
+    projectYN = input("Do you have tests (yes or no): ")
 
     if projectYN == "yes":
         x = 1
     else:
         x = 2
 
-# Final
-finalYN = input("Do you have quizzes (yes or no): ")
+    # Final
+    finalYN = input("Do you have quizzes (yes or no): ")
 
     if finalYN == "yes":
         x = 1
