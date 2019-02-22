f110e5402433b9b290b06c03e45ba3f58b4ffc42
Matthew Q. Knipfer <mknipfer@highpoint.edu>
2019-02-21 23:50:04 -0500

    Create c file version

---
+++ cruncher.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main() {
+
+    // Do you have these segments?
+
+
+    // Final
+
+
+    // Tests
+
+
+    // Quizzes
+
+
+    // Projects
+
+
+    // Homework
+
+
+    // Participation
+
+
+    // Attendance
+
+
+    // Something else?
+
+
+    return 0;
+}
