a80a323fe98bf6ba66fb0e62ea6d56d0a7cf6378
Matthew Q. Knipfer <mknipfer@highpoint.edu>
2019-02-22 23:13:12 -0500

    Create test for final existence

--- cruncher.c
+++ cruncher.c
@@ -2,9 +2,18 @@
 int main() {
 
     // Do you have these segments?
-
+    
 
     // Final
+    printf("Do you have a final (y/n): ");
+
+    char finalYN;
+    scanf("%c", &finalYN);
+    
+    while((finalYN != 'y') && (finalYN != 'n')) {
+        printf("Oops! Please enter y or n: ");
+        scanf("%c", &finalYN);
+    }
 
 
     // Tests
