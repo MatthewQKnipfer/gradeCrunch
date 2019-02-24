5cd0af1741d1564eb06dd1f8d5dcce7f50a1db74
Matthew Q. Knipfer <mknipfer@highpoint.edu>
2019-02-24 12:48:13 -0500

    Create introduction with exit option

--- cruncher.c
+++ cruncher.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main() {
 
+    // Welcome
+    printf("\n\nWelcome to GradeCrunch!");
+    printf("\n\nThis program asks some questions about your class grading criteria, followed by how you've scored so far.");
+    printf("\nGradeCrunch then calculates your running grade, followed by potential outcomes based on your upcoming tasks.\n\n");
+
+    printf("Would you like to continue? (y/n): ");
+
+    char cont;
+    scanf("%c", &cont);
+
+    while((cont != 'y') && (cont != 'n')) {
+        printf("Oops! Please enter y or n: ");
+        scanf("%c", &cont);
+    }
+
+    if (cont == 'n') {
+
+        printf("\nAlright, have a great day!\n\n");
+        exit(1);
+    }
+
     // Do you have these segments?
-    
+    printf("Let's get into it.\n\n\n");
 
     // Final
     printf("Do you have a final (y/n): ");
